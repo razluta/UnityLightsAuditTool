3ef72a2b50073f8915b4c6231c600a1a3937c25d
RAZ-PERSONAL-WI\razva <razvan.luta@gmail.com>
2023-03-20 23:02:51 -0700

    The tool is now functional, but populates the UI with dummy data.

---
+++ Editor/LightUtils.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace UnityLightsAuditTool.Editor
+{
+    public static class LightUtils
+    {
+        public static Light[] GetAllLights()
+        {
+            return UnityEngine.Object.FindObjectsOfType<Light>();
+        }
+    }
+
+    public struct LightAuditorParameters
+    {
+        public bool IsRealtime;
+        public bool IsMixed;
+        public bool IsBaked;
+        
+        private const bool IsRealtimeDefault = true;
+        private const bool IsMixedDefault = false;
+        private const bool IsBakedDefault = false;
+
+        public void Reset()
+        {
+            IsRealtime = IsRealtimeDefault;
+            IsMixed = IsMixedDefault;
+            IsBaked = IsBakedDefault;
+        }
+    }
+}
--- Editor/UnityLightsAuditToolWindow.cs
+++ Editor/UnityLightsAuditToolWindow.cs
@@ -1,13 +1,22 @@
-#if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace razluta
+namespace UnityLightsAuditTool.Editor
 {
     public class UnityLightsAuditToolWindow : EditorWindow
     {
         public VisualTreeAsset rootVisualTreeAsset;
+
+        private Light[] _lights;
+        private Toggle _realtimeTg;
+        private Toggle _mixedTg;
+        private Toggle _bakedTg;
+        private Button _resetParametersBt;
+        private Button _auditSceneBt;
+        private Button _clearAuditResultsBt;
+        private ScrollView _resultsSv;
 
         [MenuItem("Raz's Tools/Lights Audit Tool")]
         public static void ShowWindow()
@@ -21,13 +30,79 @@ namespace razluta
         public void OnEnable()
         {
             // Load and clone the window structure
-            var root = rootVisualElement;
             rootVisualTreeAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UnityLightsAuditTool/Editor/Resources/UnityLightsAuditToolRoot.uxml");
-            rootVisualTreeAsset.CloneTree(root);
+            rootVisualTreeAsset.CloneTree(rootVisualElement);
             
-            // Create a table
+            // Find / query elements
+            _realtimeTg = rootVisualElement.Q<Toggle>("Tg_Realtime");
+            _mixedTg = rootVisualElement.Q<Toggle>("Tg_Mixed");
+            _bakedTg = rootVisualElement.Q<Toggle>("Tg_Baked");
+            _resetParametersBt = rootVisualElement.Q<Button>("Bt_ResetParameters");
+            _auditSceneBt = rootVisualElement.Q<Button>("Bt_AuditScene");
+            _clearAuditResultsBt = rootVisualElement.Q<Button>("Bt_ClearResults");
+            _resultsSv = rootVisualElement.Q<ScrollView>("Sv_Results");
             
+            // Define behaviour
+            // _resetParametersBt.clickable.clicked += () => Something();
+            _auditSceneBt.clickable.clicked += () => AuditScene();
+            _clearAuditResultsBt.clickable.clicked += () => ClearResultsVisualElement();
+        }
+
+        private void AuditScene()
+        {
+            // Collect UI information
+            LightAuditorParameters lightAuditorParameters = new LightAuditorParameters();
+            lightAuditorParameters.Reset();
+            // lightAuditorParameters.HumanoidHeight = 
+            lightAuditorParameters.IsRealtime = _realtimeTg.value;
+            lightAuditorParameters.IsMixed = _mixedTg.value;
+            lightAuditorParameters.IsBaked = _bakedTg.value;
+            
+            // Clear dynamic pieces of theUI
+            ClearResultsVisualElement();
+            
+            // Update data and repopulate UI
+            PopulateUiWithData(lightAuditorParameters);
+        }
+
+        private void ClearResultsVisualElement()
+        {
+            _resultsSv.Clear();
+        }
+        
+        private void PopulateUiWithData(LightAuditorParameters lightAuditorParameters)
+        {
+            var allLights = UnityLightsAuditTool.Editor.LightUtils.GetAllLights();
+            var validLights = new List<Light>();
+            foreach (var light in allLights)
+            {
+                if ((lightAuditorParameters.IsRealtime && light.lightmapBakeType == LightmapBakeType.Realtime) || 
+                    (lightAuditorParameters.IsMixed && light.lightmapBakeType == LightmapBakeType.Mixed) || 
+                    (lightAuditorParameters.IsBaked && light.lightmapBakeType == LightmapBakeType.Baked)) 
+                    validLights.Add(light);
+            }
+
+            _lights = validLights.ToArray();
+
+            VisualTreeAsset rowAuditResult = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/UnityLightsAuditTool/Editor/Resources/Ve_AuditResult.uxml");
+            foreach (var light in _lights)
+            {
+                // Clone the visual element
+                var rowAuditResultVe = rowAuditResult.CloneTree();
+
+                // Query the visual element pieces
+                var auditResultBt = rowAuditResultVe.Q<Button>("Bt_AuditResult");
+                var lightNameLb = rowAuditResultVe.Q<Label>("Lb_Ar_Name");
+
+                // Define behavior
+                auditResultBt.clickable.clicked += () => EditorGUIUtility.PingObject(light);
+                
+                // Update the row based on the light info
+                lightNameLb.text = light.name;
+
+                // Add the row to the container
+                _resultsSv.contentContainer.Add(rowAuditResultVe);
+            }
         }
     }
 }
-#endif
